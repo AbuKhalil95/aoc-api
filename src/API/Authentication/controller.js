eee7b3577096f46b7aa4bd78dcb2673ba4924b3c
Yahya Abu Khalil <yahya_abukhaleel@hotmail.com>
2022-02-02 15:51:52 +0200

    cleaning console.log

--- src/API/Authentication/controller.js
+++ src/API/Authentication/controller.js
@@ -3,10 +3,8 @@ import { loginName } from "../../Services/Authentication";
 
 // TODO: refactor to use username/email and password once user model is made
 export const handleLogin = async (req, res) => {
-  console.log(req.query, req.params.user);
   const name = req.query.name;
   const model = authModelRoute[req.params.user];
-  console.log(model);
 
   try {
     const tokenizedInfo = await loginName(name, model);
